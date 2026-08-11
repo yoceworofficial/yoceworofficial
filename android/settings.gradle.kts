e122ba7ad1dc7eec00c238b124d61a18420010ca
yoceworofficial <yoceworofficial@gmail.com>
2026-08-11 19:48:19 +0530

    Fix Android settings imports

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode
+
 pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
 dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
 rootProject.name = "YOCEWOR"
