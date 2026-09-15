9ca74a5b38c4c54471a6c77e353e7641cea58e3d
yoceworofficial <yoceworofficial@gmail.com>
2026-09-15 21:16:17 +0530

    Remove old site files for complete rebuild

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='yocewor-disabled-sw-v1';
-self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())await caches.delete(k);await self.clients.claim()})()));
-self.addEventListener('fetch',e=>{});
